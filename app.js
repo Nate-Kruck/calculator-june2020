d0a4687c2b2dac5d2e4e2e61cb22ad7d49255dbe
Nathan Kruckenberg <natekruck1990@gmail.com>
2020-06-25 14:23:46 -0700

    added the multiply and divide functions

--- app.js
+++ app.js
@@ -46,8 +46,52 @@ minusButton.addEventListener('click', () => {
     const value3 = subInput1.value;
     const value4 = subInput2.value;
 
-    // add the two values together
+    // subtract the two values together
     const sub = Number(value3) - Number(value4);
 
     minusSpan.textContent = sub;
+})
+
+// get multiply-input-1
+const multiplyInput1 = document.getElementById('multiply-input-1');
+// get multiply-input-2
+const multiplyInput2 = document.getElementById('multiply-input-2');
+// get multiply-button
+const multiplyButton = document.getElementById('multiply-button');
+//get multiply-result span
+const multiplySpan = document.getElementById('multiply-result');
+
+// initialize state
+
+// set the event handler to the button
+multiplyButton.addEventListener('click', () => {
+    const value5 = multiplyInput1.value;
+    const value6 = multiplyInput2.value;
+
+    // multiply the two values together
+    const multiply = Number(value5) * Number(value6);
+
+    multiplySpan.textContent = multiply;
+})
+
+// get divide-input-1
+const divideInput1 = document.getElementById('divide-input-1');
+// get divide-input-2
+const divideInput2 = document.getElementById('divide-input-2');
+// get divide-button
+const divideButton = document.getElementById('divide-button');
+// get divide-result span
+const divideSpan = document.getElementById('divide-result');
+
+// initialize state
+
+// set the event handler to the button
+divideButton.addEventListener('click', () => {
+    const value7 = divideInput1.value;
+    const value8 = divideInput2.value;
+
+    // divide the two values together
+    const divide = Number(value7) / Number(value8);
+
+    divideSpan.textContent = divide;
 })
